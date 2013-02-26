180e840712b81f4dd03b8015e4694a73919a5005
Great-Beaver <greatbeavers@gmail.com>
2013-02-26 23:11:26 +0600

    Add id and 1 overload for SendPacket

--- ChatClient/Ports.cs
+++ ChatClient/Ports.cs
@@ -13,12 +13,12 @@ namespace ChatClient
         private bool _continue = false;
         private SerialPort _comPort;
         private Thread _readThread;
-        private int _clietnId ;
+        private byte _clietnId;
         private byte[] _readBufferHeader = new byte[6];
         private Crc16 _crc16 = new Crc16();
         byte[] _reciveMessageLenght;
 
-        public Ports ( string comPortName, int id)
+        public Ports(string comPortName, byte id)
         {
             _comPort = new SerialPort();
             _comPort.PortName = comPortName;
@@ -40,12 +40,17 @@ namespace ChatClient
             _readThread.Start();
         }
 
-
-        public void SendPacket(string message)
+        public void SendPacket(string message, byte toId)
         {
-            // Переводим строку в массив байтов 
+        // Переводит строку в массив байтов 
             byte[] messageBody = Encoding.UTF8.GetBytes(message);
-            
+
+            SendPacket(messageBody,toId);
+        }
+
+
+        public void SendPacket(byte[] messageBody, byte toId)
+        {                
             //Массив всего выходного пакета, Header + тело сообщения(Data)
             byte[] outPacket = new byte[10+messageBody.Length];
 
@@ -60,25 +65,25 @@ namespace ChatClient
             outPacket[1] = 0x55;
 
             //Адрес получателя
-            outPacket[2] = 0x00;
+            outPacket[2] = toId;
 
             //Адрес отправителя
-            outPacket[3] = 0x00;
+            outPacket[3] = _clietnId;
 
-            // Копируем тело сообщения в позицию после Header'а
+            // Копирует тело сообщения в позицию после Header'а
             Array.Copy(messageBody,0,outPacket,10,messageBody.Length);
 
-            // Вставляем длинну сообщения тела сообщения в Header'а
+            // Вставляет длинну сообщения тела сообщения в Header'а
             Array.Copy(BitConverter.GetBytes(((short)messageBody.Length)), 0, packetWithOutHash, 2, 2);
 
-            // Выставляем опции в Header
+            // Выставляет опции в Header
             packetWithOutHash[4] = 0x48;
             packetWithOutHash[5] = 0x49;
 
-            // Вставляем header без CRC в начало пакета 
+            // Вставляет header без CRC в начало пакета 
             Array.Copy(packetWithOutHash, 0, outPacket, 2, 6);
 
-            // Вычисляем и вставляет CRC Header'а в пакет
+            // Вычисляет и вставляет CRC Header'а в пакет
             Array.Copy(_crc16.ComputeChecksumBytes(packetWithOutHash),0,outPacket,8,2);
 
             // just hint to array copy
@@ -90,12 +95,7 @@ namespace ChatClient
            // 0 = start index in destination array
            // 3 = elements to copy
 
-         //  MessageBox.Show(System.Text.Encoding.UTF8.GetString(outPacket));
-         //
-         //  MessageBox.Show(Convert.ToString(BitConverter.ToInt16(packetWithOutHash, 2)));
-
-
-
+            // Отправляет пакет
             _comPort.Write(outPacket, 0, outPacket.Length);
 
         }
@@ -104,66 +104,49 @@ namespace ChatClient
         {
             while (_continue)
             {
+                    //Если найдена сигнатура начинается обработка пакета
                 if (_comPort.BytesToRead > 1 && _comPort.ReadByte() == 0xAA && _comPort.ReadByte() == 0x55)
                 {
+                    // Если количество входныъ байтов равно или более количества байтов в Header'е
                     if (_comPort.BytesToRead > 7)
                     {
+                        // Debug message
                         MessageBox.Show("Ok");
 
+                        // Считывает header без CRC 
                         byte[] messageHeaderWithoutHash = new byte[6];
                         _comPort.Read(messageHeaderWithoutHash, 0, 6);
 
-                      //  MessageBox.Show("Calculated hash = " + Convert.ToString(_crc16.ComputeChecksum(messageHeaderWithoutHash))
-                      //      + "Recived hash = " + Convert.ToString(BitConverter.ToUInt16(new byte[] { (byte)_comPort.ReadByte(), (byte)_comPort.ReadByte() }, 0)));
-
-                        ushort lenght =
-                            BitConverter.ToUInt16(new byte[] { messageHeaderWithoutHash[2], messageHeaderWithoutHash[3] }, 0);
-
-                        MessageBox.Show("Lenght of recived message " + lenght.ToString());
-
+                        // Сверка CRC
                         if (_crc16.ComputeChecksum(messageHeaderWithoutHash) == BitConverter.ToUInt16(new byte[] { (byte)_comPort.ReadByte(), (byte)_comPort.ReadByte() }, 0))
                        {
+                            // Debug message
                            MessageBox.Show("Hash matches!");
-                          // short lenght =
-                         //   BitConverter.ToInt16(new byte[] { messageHeaderWithoutHash[0], messageHeaderWithoutHash[1] }, 0);
 
+                            // Получает значение длинны тела сообщения
+                           ushort lenght =
+                          BitConverter.ToUInt16(new byte[] { messageHeaderWithoutHash[2], messageHeaderWithoutHash[3] }, 0);
+
+                            // Debug message
+                           MessageBox.Show("Lenght of recived message " + lenght.ToString());
+
+                            // Считывает тело сообщения 
                            byte[] messageBody = new byte[lenght];
-
                            _comPort.Read(messageBody, 0, lenght);
 
+                            // Debug message
                            MessageBox.Show(Encoding.UTF8.GetString(messageBody));
-
-
-
-
                        }
                        else
                        {
+                          // Debug message
                           MessageBox.Show("Hash NOT matches!"); 
                        }
                          
-                        
-                
-
-              //
-              //          short lenght =
-              //              BitConverter.ToInt16(new byte[] {(byte) _comPort.ReadByte(), (byte) _comPort.ReadByte()}, 0);
-              //          _comPort.ReadByte();
-              //          _comPort.ReadByte();
-              //          MessageBox.Show(lenght.ToString());
-              //          MessageBox.Show(
-              //              _crc16.ComputeChecksum(new byte[] {(byte) _comPort.ReadByte(), (byte) _comPort.ReadByte()})
-              //                    .ToString());
-              //
-              //          //_comPort.Read(_reciveMessageLenght, 0, 2);
-              //          // = BitConverter.ToInt16(_reciveMessageLenght);
-              //
-
                     }
                     Thread.Sleep(500);
                 }
             }
-
 
         }
 
